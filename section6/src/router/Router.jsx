57279f2599b6a1db966e7a0970e5b2e8ad615e1e
teraiken <terai.ken@relation2012.com>
2024-01-24 20:41:27 +0900

    section6

---
+++ section6/src/router/Router.jsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Top } from "../components/pages/Top";
+import { Users } from "../components/pages/Users";
+import { DefaultLayout } from "../components/templates/DefaultLayout";
+import { HeaderOnly } from "../components/templates/HeaderOnly";
+
+export const Router = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route
+                    exact
+                    path="/"
+                    element={
+                        <DefaultLayout>
+                            <Top />
+                        </DefaultLayout>
+                    }
+                />
+
+                <Route
+                    exact
+                    path="/users"
+                    element={
+                        <HeaderOnly>
+                            <Users />
+                        </HeaderOnly>
+                    }
+                />
+            </Routes>
+        </BrowserRouter>
+    );
+}
