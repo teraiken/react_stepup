5f31b2a6eb9c8902ef7d25849e99cea5e3553186
teraiken <terai.ken@relation2012.com>
2024-01-25 21:27:22 +0900

    format App

--- section9/src/App.tsx
+++ section9/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Practice1 } from "./practices/Practice1";
 import { Practice2 } from "./practices/Practice2";
